4a6ff9a2b748f4cbb705d05fd0a0e89a246e5d7e
Даниил Куксарь <dan.kukar@yandex.ru>
2024-11-27 00:54:11 +0300

    bugfix: logs was added to detect problem on github

--- server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
+++ server/src/main/java/ru/practicum/shareit/booking/controller/BookingController.java
@@ -25,7 +25,10 @@ public class BookingController {
             @RequestHeader(Constants.USER_ID_HEADER) long ownerId,
             @RequestBody BookingRequestDto bookingRequestDto) {
         log.info("[SERVER | CONTROLLER] Received request to create booking by ownerId='{}'", ownerId);
-        return bookingService.createBooking(bookingRequestDto, ownerId);
+        BookingResponseDto response = bookingService.createBooking(bookingRequestDto, ownerId);
+        log.info("[SERVER | CONTROLLER] Booking successfully created and saved in db. ID = {}", response.getId());
+        log.debug("[SERVER | CONTROLLER] Booking with id '{}' was received with data: {}", response.getId(), response);
+        return response;
     }
 
     @PatchMapping("/{bookingId}")
@@ -33,27 +36,34 @@ public class BookingController {
             @PathVariable(value = "bookingId") long bookingId,
             @RequestHeader(Constants.USER_ID_HEADER) long ownerId,
             @RequestParam("approved") boolean isApproved) {
-        log.info("[SERVER | CONTROLLER] Request to approve?='{}' booking by bookingId='{}' and ownerId='{}'",
-                isApproved, bookingId, ownerId);
-        return bookingService.approveBooking(bookingId, ownerId, isApproved);
+        log.info("[SERVER | CONTROLLER] Received request to approve booking by bookingId='{}' with approval status='{}' and ownerId='{}'",
+                bookingId, isApproved, ownerId);
+        BookingResponseDto response = bookingService.approveBooking(bookingId, ownerId, isApproved);
+        log.info("[SERVER | CONTROLLER] Booking approval processed for bookingId='{}' with approval status='{}'", bookingId, isApproved);
+        log.debug("[SERVER | CONTROLLER] Booking approval response for bookingId='{}': {}", bookingId, response);
+        return response;
     }
 
     @GetMapping
     public Collection<BookingResponseDto> getAllBookingsOfRenter(
             @RequestHeader(Constants.USER_ID_HEADER) long renterId,
             @RequestParam(defaultValue = "ALL") String status) {
-
-        log.info("[SERVER | CONTROLLER] Request to get bookings by renterId='{}' and bookingStatus='{}'", renterId, status);
-        return bookingService.getAllByRenterIdAndFindStatus(renterId, BookingFindStatus.valueOf(status));
+        log.info("[SERVER | CONTROLLER] Request to get bookings for renterId='{}' with bookingStatus='{}'", renterId, status);
+        Collection<BookingResponseDto> bookings = bookingService.getAllByRenterIdAndFindStatus(renterId, BookingFindStatus.valueOf(status));
+        log.info("[SERVER | CONTROLLER] Fetched {} bookings for renterId='{}' with status='{}'", bookings.size(), renterId, status);
+        log.debug("[SERVER | CONTROLLER] Bookings for renterId='{}' with status='{}': {}", renterId, status, bookings);
+        return bookings;
     }
 
     @GetMapping("/owner")
     public Collection<BookingResponseDto> getAllBookingsOfItemOwner(
             @RequestHeader(Constants.USER_ID_HEADER) long ownerId,
             @RequestParam(defaultValue = "ALL") String status) {
-
-        log.info("[SERVER | CONTROLLER] Request to get bookings by ownerId='{}' and bookingStatus='{}'", ownerId, status);
-        return bookingService.getAllByOwnerIdAndFindStatus(ownerId, BookingFindStatus.valueOf(status));
+        log.info("[SERVER | CONTROLLER] Request to get bookings for ownerId='{}' with bookingStatus='{}'", ownerId, status);
+        Collection<BookingResponseDto> bookings = bookingService.getAllByOwnerIdAndFindStatus(ownerId, BookingFindStatus.valueOf(status));
+        log.info("[SERVER | CONTROLLER] Fetched {} bookings for ownerId='{}' with status='{}'", bookings.size(), ownerId, status);
+        log.debug("[SERVER | CONTROLLER] Bookings for ownerId='{}' with status='{}': {}", ownerId, status, bookings);
+        return bookings;
     }
 
     @GetMapping("/{bookingId}")
@@ -61,6 +71,9 @@ public class BookingController {
             @PathVariable long bookingId,
             @RequestHeader(Constants.USER_ID_HEADER) long userId) {
         log.info("[SERVER | CONTROLLER] Request to get booking by bookingId='{}' and userId='{}'", bookingId, userId);
-        return bookingService.getBookingByIdAndUserId(bookingId, userId);
+        BookingResponseDto booking = bookingService.getBookingByIdAndUserId(bookingId, userId);
+        log.info("[SERVER | CONTROLLER] Fetched booking with bookingId='{}' for userId='{}'", bookingId, userId);
+        log.debug("[SERVER | CONTROLLER] Booking details for bookingId='{}': {}", bookingId, booking);
+        return booking;
     }
 }
